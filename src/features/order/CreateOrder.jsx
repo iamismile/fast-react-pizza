3e32a3346d4451e26891cd0f8f9d2a088cc03f3f
Ismile Hossain <iamismile.dev@gmail.com>
2024-01-27 00:41:27 +0600

    user state with redux

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -33,6 +34,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   const navigation = useNavigation();
   const formErrors = useActionData();
 
@@ -52,6 +54,7 @@ function CreateOrder() {
           <input
             type="text"
             name="customer"
+            defaultValue={username}
             required
             className="input flex-grow"
           />
