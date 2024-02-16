93bdd6b6c0f82eb5ffc9ebd068ae697bc838001f
Ismile Hossain <iamismile.dev@gmail.com>
2024-02-16 11:52:01 +0600

    fix async thunk issue

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -47,13 +47,15 @@ const userSlice = createSlice({
         state.status = "loading";
       })
       .addCase(fetchAddress.fulfilled, (state, action) => {
-        state.position = action.position;
-        state.address = action.address;
+        state.position = action.payload.position;
+        state.address = action.payload.address;
         state.status = "idle";
       })
+      // eslint-disable-next-line no-unused-vars
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = "error";
-        state.error = action.error.message;
+        state.error =
+          "There was a problem getting your address. Make sure to fill this field!";
       }),
 });
 
